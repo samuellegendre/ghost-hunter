61ed3fe7dc9832d4e76959bb78d6f8b41e08877c
Samuel Legendre <samuel.legendre@protonmail.com>
2021-12-11 12:05:59 -0500

    Add publisher.py

---
+++ lib/publisher.py
@@ -0,0 +1,16 @@
+class Publisher:
+    def __init__(self, events):
+        self.events = {event: dict() for event in events}
+
+    def get_subscribers(self, event):
+        return self.events[event]
+
+    def register(self, event, subscriber, callback):
+        self.get_subscribers(event)[subscriber] = callback
+
+    def unregister(self, event, subscriber):
+        del self.get_subscribers(event)[subscriber]
+
+    def dispatch(self, event):
+        for subscriber, callback in self.get_subscribers(event).items():
+            callback()
